3afc1b887acd7aba4b7dcb0125d33a51728cae80
Muchacho <Muchacho.ps@gmail.com>
2016-06-10 19:02:02 +0200

    Same LearnController

--- Colander/Controllers/LearnController.cs
+++ Colander/Controllers/LearnController.cs
@@ -2,6 +2,7 @@
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net;
 using System.Web;
 using System.Web.Mvc;
 
@@ -10,10 +11,29 @@ namespace Colander.Controllers
     public class LearnController : Controller
     {
         private WordListDBContext db = new WordListDBContext();
+        private WordService.WordService wordService = new WordService.WordService();
+
+
+
         // GET: Learn
-        public ActionResult Learn()
+        public ActionResult Learn(int? id)
         {
-            return View();
+            if (id == null)
+            {
+
+                //id = wordService.CurrentListID;
+
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+
+            }
+            //Word word = db.Words.Find(id);
+            wordService.CurrentListID = (int)id;
+            var word = wordService.GetForWordId(id);
+            if (word == null)
+            {
+                return HttpNotFound();
+            }
+            return View(word);
         }
     }
 }
--- Colander/Controllers/WordsController.cs
+++ Colander/Controllers/WordsController.cs
@@ -139,26 +139,6 @@ namespace Colander.Controllers
             return RedirectToAction("Index");
         }
 
-        // GET: Learn
-        public ActionResult Learn(int? id)
-        {
-            if (id == null)
-            {
-
-                //id = wordService.CurrentListID;
-
-                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
-
-            }
-            //Word word = db.Words.Find(id);
-            wordService.CurrentListID = (int)id;
-            var word = wordService.GetForWordId(id);
-            if (word == null)
-            {
-                return HttpNotFound();
-            }
-            return View(word);
-        }
 
         protected override void Dispose(bool disposing)
         {
