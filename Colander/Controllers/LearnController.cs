1ebaa6c3f6bd3d4d4c3fb45d5cd9d6fbd57f6459
Agszu <agataszulc97@gmail.com>
2016-06-11 09:42:00 +0200

    sciagnelam pliki, a kaze mi commitowac

--- Colander/Controllers/LearnController.cs
+++ Colander/Controllers/LearnController.cs
@@ -13,11 +13,13 @@ namespace Colander.Controllers
         private WordListDBContext db = new WordListDBContext();
         private WordService.WordService wordService = new WordService.WordService();
         public string usersAnswer;
-        
+
+
+        //Random words 
         // GET: Learn
         public ActionResult Learn(int? id)
         {
-            
+
             if (id == null)
             {
 
@@ -27,16 +29,53 @@ namespace Colander.Controllers
 
             }
             //Word word = db.Words.Find(id);
-            wordService.CurrentListID = id;
+            //wordService.CurrentListID = id;
+            //var word = wordService.GetForWordId(id);
+            Random random = new Random();
+            var words = wordService.GetForListId(id);
+            Word word = null;
+
+            while (words.Any() && word == null)
+            {
+                word = words.ElementAt(random.Next(words.Count()));
+            }
+
+            if (word == null)
+            {
+                
+                return HttpNotFound();
+            }
+
+
+            return View(word);
+
+        }
+
+
+        //Specified word
+        // GET: LearnWord
+
+        public ActionResult LearnWord(int? id)
+        {
+
+            if (id == null)
+            {
+
+                //id = wordService.CurrentListID;
+
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+
+            }
+            //Word word = db.Words.Find(id);
+            //wordService.CurrentListID = id;
             var word = wordService.GetForWordId(id);
             if (word == null)
             {
                 return HttpNotFound();
             }
-            return View(word);
-            
 
-            
+            return View(word);
+
         }
     }
 }
